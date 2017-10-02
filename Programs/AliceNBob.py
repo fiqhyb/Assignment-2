8209cfda1edd60349fb160a3167fb3fe1213bc70
Fiqhy Bismadhika <fiqhy.bismadhika@binus.ac.id>
2017-10-02 21:00:22 +0700

    Second Assignment

--- Programs/AliceNBob.py
+++ Programs/AliceNBob.py
@@ -4,6 +4,6 @@ def ANB():####Game Of Stones####
     ston=int(input("#############################\nNumber of stones on ground  :"))
     if ston==0:print("There is no stones")
     elif ston<0:print(h.a)
-    elif ston % 2==0:print("Alice wins")
-    else:print("Bob Wins")
+    elif ston / 2==0:print("Bob wins")
+    else:print("Alice Wins")
     time.sleep(3)
